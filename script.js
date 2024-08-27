1c130f491f8a33f0afe49c4ea1f294d0b66a70c1
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-08-27 17:12:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 window.promises = [];
 
+
+const promises = Array.from({ length: 5 }, () => {
+    const delay = Math.floor(Math.random() * 5000) + 1000; 
+    return new Promise(resolve => setTimeout(() => resolve(delay), delay));
+});
+
+
+Promise.any(promises)
+    .then(result => {
+     
+        document.getElementById('output').innerText = `First resolved promise result: ${result} ms`;
+    })
+
+        document.getElementById('output').innerText = `No promise resolved: ${error}`;
+    });
+
 // Do not change the code above this
 // add your promises to the array `promises`
